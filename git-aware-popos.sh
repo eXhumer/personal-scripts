322fb1b207645d8e264160a630149572a769007c
eXhumer <62310242+eXhumer@users.noreply.github.com>
2020-04-24 23:13:53 -0600

    Comment Typo Fix

--- git-aware-popos.sh
+++ git-aware-popos.sh
@@ -11,7 +11,7 @@ export GIT_PS1_SHOWUNTRACKEDFILES=true
 export GIT_PS1_SHOWUPSTREAM="auto"
 export GIT_PS1_SHOWCOLORHINTS=true
 
-# SET PROMPRT_COMMAND VARIABLE BASED ON WHETHER color_prompt IS SET OR NOT
+# SET PROMPT_COMMAND VARIABLE BASED ON WHETHER color_prompt IS SET OR NOT
 if [ "$color_prompt" = yes ]; then
     PROMPT_COMMAND='__git_ps1 "${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]" "\$ "'
 else
